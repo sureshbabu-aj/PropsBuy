65fbadba740f1da03da9207222e7568d41a91ce0
DESKTOP-4B36RGF\Home <sureshbabu.aj@gmail.com>
2024-12-01 12:14:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,18 +17,24 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // MySQL database connection
-const db = mysql.createConnection({
+
+
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('MySQL connected...');
-});
+
+
+
+
+
 
 // Attach the database connection to request object
 app.use((req, res, next) => {
@@ -49,59 +55,4 @@ app.use('/api/pb', props_details);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
-
-
-
-
-// Function to get public IP
-/*
-function getPublicIP(callback) {
-    http.get({ host: 'checkip.amazonaws.com', port: 80, path: '/' }, function (res) {
-        res.on('data', function (ip) {
-            callback(ip.toString().trim());
-        });
-    });
-}
-*/
-/*
-
-// Function to fetch public IP
-const getPublicIP = async () => {
-  try {
-    const { data } = await axios.get('https://api.ipify.org?format=json');
-    return data.ip;
-  } catch (error) {
-    console.error('Could not fetch public IP:', error);
-    return null;
-  }
-};
-
-
-// Start server
-
-const HOST = process.env.HOST || '0.0.0.0'; // Default to all interfaces
-app.listen(PORT, HOST, async () => {
-  if (HOST === '0.0.0.0') {
-    console.log(`Server running at http://localhost:${PORT}`);
-    const publicIP = await getPublicIP();
-    if (publicIP) {
-      console.log(`Server also accessible at http://${publicIP}:${PORT}`);
-    }
-  } else {
-    console.log(`Server running at http://${HOST}:${PORT}`);
-  }
-});
-*/
-
-
-
-/*
-app.listen(PORT, '0.0.0.0', () => {
-   getPublicIP((publicIP) => {
-        console.log(`Server running at http://${publicIP}:${PORT}`);
-    });
-});
-*/
 
